8e3611c0aee169909bcdd5cebe24b82304dc2a56
hactrox <hactrox@gmail.com>
2020-12-09 16:48:11 +0800

    Update centos7_install.sh

--- centos7_install.sh
+++ centos7_install.sh
@@ -108,7 +108,8 @@ cd ~/downloads
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
 # Copy cloud watch configuration from aws console to ~/downloads/config.json
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:~/downloads/config.json
+sudo yum -y install collectd
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:config.json
 systemctl status amazon-cloudwatch-agent
 
 # Install AWS CodeDeploy
