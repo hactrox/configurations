d3296f577ac187da9d721f2f95099d514bb97858
hactrox <hactrox@gmail.com>
2020-12-09 16:27:06 +0800

    Update centos7_install.sh

--- centos7_install.sh
+++ centos7_install.sh
@@ -17,6 +17,9 @@ sudo reboot
 # Update softwares
 sudo yum -y update
 
+# Add epel repo
+sudo yum install epel-release -y
+
 # Enable bbr
 echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
