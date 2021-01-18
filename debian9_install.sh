772bd0f5213b12c73aa4334905a982a65f8bfef6
hactrox <hactrox@gmail.com>
2021-01-18 10:40:03 +0800

    docs: add golang installation script

--- debian9_install.sh
+++ debian9_install.sh
@@ -15,3 +15,13 @@ sudo apt -y install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat
 v=2.29.2;wget https://www.kernel.org/pub/software/scm/git/git-$v.tar.gz -O git-$v.tar.gz && tar -zxf git-$v.tar.gz && cd git-$v; unset v
 make configure && ./configure --prefix=/usr && make && sudo make install
 git --version
+
+# Install golang
+mkdir -p ~/downloads
+cd ~/downloads
+v=1.15.6;wget https://golang.org/dl/go$v.linux-amd64.tar.gz && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf go$v.linux-amd64.tar.gz
+echo 'PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
+source /etc/profile
+go version
