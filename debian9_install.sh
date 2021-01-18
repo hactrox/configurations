3afec4c995ef53726f242f00bd00b5ba7c4e7255
hactrox <hactrox@gmail.com>
2021-01-18 10:38:06 +0800

    docs: add git installation script

--- debian9_install.sh
+++ debian9_install.sh
@@ -6,3 +6,12 @@ sudo apt update && sudo apt -y upgrade
 
 # Prepare downloads dir
 mkdir -p ~/downloads
+
+# Install git
+mkdir -p ~/downloads
+cd ~/downloads
+sudo apt update
+sudo apt -y install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip autoconf
+v=2.29.2;wget https://www.kernel.org/pub/software/scm/git/git-$v.tar.gz -O git-$v.tar.gz && tar -zxf git-$v.tar.gz && cd git-$v; unset v
+make configure && ./configure --prefix=/usr && make && sudo make install
+git --version
